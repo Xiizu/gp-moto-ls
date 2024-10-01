52f13cc3ec87fba894025c74185d8dcecb50399e
Lounes <lounes.skr@gmail.com>
2024-10-01 15:34:34 +0200

    Lien circuit affiche la liste des circuits

--- controller/circuitsController.php
+++ controller/circuitsController.php
@@ -1,30 +1,19 @@
 <?php
 function connect(){
-    try {
-        $host = "127.0.0.1";
-        $database = "gp_moto";
-        $bdd = 'mysql:host='+$host+';dbname='+$database+';charset=utf8';
-        $username = 'projetgit';
-        $password = 'projetgit';
-        
-        $pdo = new PDO($bdd, $username, $password);
-        return $pdo;
-    } catch (PDOException $e) {
-        die('Erreur : ' . $e->getMessage());
-    }
+    $bdd = 'mysql:host=127.0.0.1;dbname=gp_moto;charset=utf8';
+    $username = 'projetgit';
+    $password = 'projetgit';
+    $pdo = new PDO($bdd, $username, $password);
+    return $pdo;
 }
 
-function select($info="*", $condition="True"){
-    try {
-        $sql = "SELECT $info FROM circuit WHERE $condition";
-        $requette = connect()->prepare($sql);
-        $requette->execute();
-        $resultats = $requette->fetchAll();
-        return $resultats ;
-    }
-    catch (PDOException $e) {
-    echo "Erreur lors de la récupération des données : " . $e->getMessage();
-}}
+function select($info="id,nom,pays,annee,longueur", $condition="True"){
+    $sql = "SELECT $info FROM circuit WHERE $condition";
+    $requette = connect()->prepare($sql);
+    $requette->execute();
+    $resultats = $requette->fetchAll(PDO::FETCH_ASSOC);
+    return $resultats ;
+}
 
 
 
--- vue/circuits.php
+++ vue/circuits.php
@@ -1,30 +1,36 @@
-<div class="circuits">
-    <table border="1">
-        <thead>
-            <tr>
-                <th>
-                    Circuit 1
-                </th>
-                <th>
-                    Circuit 2
-                </th>
-                <th>
-                    Circuit 3
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>
-                    Celui ci est cool
-                </td>
-                <td>
-                    Celui la est joli
-                </td>
-                <td>
-                    Celui la est pratique
-                </td>
-            </tr>
-        </tbody>        
-    </table>    
+<link rel="stylesheet" href="css/circuits.css">
+<div class="content">
+<?php
+require("controller/circuitsController.php");
+
+$circuits = select();
+
+echo '<div class="cartes">';
+foreach ($circuits as $circuit) {
+    echo '<div class="circuit">';
+    foreach($circuit as $cle => $valeur){
+        $text = "";
+        switch ($cle) {
+            case 'nom':
+                $text .= '<p><b>'.$valeur.'</b></p>';
+                break;
+            case 'pays':
+                $text .= '<p><i>'.$valeur.'</i></p>';
+                break;
+            case 'annee':
+                $text .= '<p>depuis '.$valeur.'</p>';
+                break;
+            case 'longueur':
+                $text .= '<p>'.$valeur.'m</p>';
+                break;
+            default:
+                break;
+        }
+        echo $text;
+    }
+    echo '</div>';
+}
+echo '</div>';
+
+?>
 </div>
